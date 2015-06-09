d9f3fd332633faf9c94a86a6984de5d48611bf41
Lee HyangSeok <eekek@naver.com>
2015-06-09 23:13:04 +0900

    vo > Member.java

---
+++ watchassembly/src/main/java/vo/Member.java
@@ -0,0 +1,25 @@
+package vo;
+
+/**
+ * Created by Administrator on 2015-06-09.
+ */
+public class Member {
+    private String id;
+    private String nickname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
