9cd7158127956a61978a727fce079f14220ebe1e
Lee HyangSeok <eekek@naver.com>
2015-06-10 16:32:10 +0900

    MainActivity, KakaoActivity 기능 분리 테스트 완료

--- watchassembly/src/main/java/com/lime/watchassembly/MainActivity.java
+++ watchassembly/src/main/java/com/lime/watchassembly/MainActivity.java
@@ -3,47 +3,60 @@ package com.lime.watchassembly;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.kakao.UserProfile;
+import com.kakao.helper.Logger;
 import com.lime.watchassembly.kakao.KakaoActivity;
+
+import vo.Member;
 
 
 public class MainActivity extends Activity {
 
-    private Button btnKakao;
-    private Button btnPreView;
-    private Button btnLogout;
+    private Button btnKakaoLogin;
+    private Button btnLoginFree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        btnKakao = (Button) findViewById(R.id.btnKakao);
-        btnKakao.setOnClickListener(new View.OnClickListener() {
+        btnKakaoLogin = (Button) findViewById(R.id.btnKakaoLogin);
+        btnKakaoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                kakaoUser("login");
+                kakaoLogin();
             }
         });
-        btnPreView = (Button) findViewById(R.id.btnPreView);
-        btnLogout = (Button) findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        btnLoginFree = (Button) findViewById(R.id.btnLoginFree);
+        btnLoginFree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                kakaoUser("logout");
+                loginFree();
             }
         });
+
+        checkLogon();
     }
 
+    private void checkLogon(){
+        UserProfile userProfile = UserProfile.loadFromCache();
+        if (userProfile != null && userProfile.getId() > 0) {
+            kakaoLogin();
+        }
+    }
 
-    private void kakaoUser(String msg){
+    private void kakaoLogin() {
         Intent intent = new Intent(this, KakaoActivity.class);
-        intent.putExtra("MSG", msg);
         startActivity(intent);
-        //finish();
+        finish();
+    }
+
+    private void loginFree() {
+
+        finish();
     }
 }
--- watchassembly/src/main/java/com/lime/watchassembly/kakao/KakaoActivity.java
+++ watchassembly/src/main/java/com/lime/watchassembly/kakao/KakaoActivity.java
@@ -3,90 +3,96 @@ package com.lime.watchassembly.kakao;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.kakao.APIErrorResult;
 import com.kakao.LogoutResponseCallback;
-import com.kakao.MeResponseCallback;
 import com.kakao.UserManagement;
 import com.kakao.UserProfile;
 import com.kakao.helper.Logger;
+import com.lime.watchassembly.MainActivity;
+import com.lime.watchassembly.R;
+
+import vo.Member;
 
 /**
  * Created by Administrator on 2015-06-09.
  */
 public class KakaoActivity extends Activity {
+
+    private Member kakaoMember;
     private UserProfile userProfile;
+
+    private Button btnLogout;
+    private TextView txtNickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeView();
+        redirectLoginActivity();
+    }
 
-        // Get the message from the intent
-        Intent intent = getIntent();
-        String message = intent.getStringExtra("MSG");
+    private void initializeView() {
+        setContentView(R.layout.kakao_main);
 
-        if (message != null) {
-            if (message.equals("login")) {
-                redirectLoginActivity();
-            } else if (message.equals("logout")) {
+        userProfile = UserProfile.loadFromCache();
+
+        txtNickname = (TextView) findViewById(R.id.txtNickname);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 redirectLogoutActivity();
             }
-            //Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-        }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        UserManagement.requestMe(new MeResponseCallback() {
-            @Override
-            protected void onSuccess(UserProfile userProfile) {
-                Toast.makeText(getApplicationContext(), userProfile.getNickname() + " 로그인 !!", Toast.LENGTH_LONG).show();
-                finish();
-            }
+        if (userProfile != null) {
+            long id = userProfile.getId();
+            String nickname = userProfile.getNickname();
 
-            @Override
-            protected void onNotSignedUp() {
-                finish();
+            if (id > 0) {
+                Logger.getInstance().d("로그인정보:" + id + "/" + nickname);
+                kakaoMember = new Member();
+                kakaoMember.setId("" + id);
+                kakaoMember.setNickname(nickname);
+                txtNickname.setText(kakaoMember.getNickname());
             }
-
-            @Override
-            protected void onSessionClosedFailure(APIErrorResult apiErrorResult) {
-                finish();
-            }
-
-            @Override
-            protected void onFailure(APIErrorResult apiErrorResult) {
-                finish();
-            }
-        });
+        }
     }
 
     private void redirectLoginActivity() {
-        Intent intent = new Intent(this, KakaoLoginActivity.class);
-        startActivity(intent);
-        //finish();
+        if (userProfile == null || userProfile.getId() < 0) {
+            Intent intent = new Intent(this, KakaoLoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
-    private void redirectSignupActivity() {
-        Intent intent = new Intent(this, KakaoSignupActivity.class);
+    private void redirectMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-        //finish();
+        finish();
     }
 
     private void redirectLogoutActivity() {
         UserManagement.requestLogout(new LogoutResponseCallback() {
             @Override
             protected void onSuccess(long l) {
-                Toast.makeText(getApplicationContext(), "로그아웃 !!", Toast.LENGTH_LONG).show();
-                finish();
+                Logger.getInstance().d("로그아웃");
+                kakaoMember = null;
+                txtNickname.setText("");
+                redirectMainActivity();
             }
 
             @Override
             protected void onFailure(APIErrorResult apiErrorResult) {
-                Toast.makeText(getApplicationContext(), "로그아웃 상태입니다.", Toast.LENGTH_LONG).show();
-                finish();
             }
         });
     }
