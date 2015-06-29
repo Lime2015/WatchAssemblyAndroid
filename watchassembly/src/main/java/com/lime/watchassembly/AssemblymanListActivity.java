da6f8089478abda416e8400f3d084a610d3f76f6
Lee HyangSeok <eekek@naver.com>
2015-06-29 17:41:31 +0900

    drawlayout testing/...

--- watchassembly/src/main/java/com/lime/watchassembly/AssemblymanListActivity.java
+++ watchassembly/src/main/java/com/lime/watchassembly/AssemblymanListActivity.java
@@ -1,6 +1,7 @@
 package com.lime.watchassembly;
 
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -8,13 +9,21 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.lime.watchassembly.R;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 
 /**
  * Created by SeongSan on 2015-06-29.
  */
-public class AssemblymanListActivity  extends ActionBarActivity {
+public class AssemblymanListActivity extends ActionBarActivity {
+
+    private String[] navItems = {"국회의원", "의안", "명예의전당", "국민참여", "마이페이지"};
+
+    private ListView lvNavList;
+    private FrameLayout flContainer;
 
     Toolbar toolbar;
     DrawerLayout dlDrawer;
@@ -26,17 +35,23 @@ public class AssemblymanListActivity  extends ActionBarActivity {
         setContentView(R.layout.activity_assemblyman_list);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        dlDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
         setSupportActionBar(toolbar);
 
+        lvNavList = (ListView) findViewById(R.id.drawer);
+        flContainer = (FrameLayout) findViewById(R.id.container);
+
+        lvNavList.setAdapter(
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navItems));
+        lvNavList.setOnItemClickListener(new DrawerItemClickListener());
+
+        dlDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         dtToggle = new ActionBarDrawerToggle(this, dlDrawer, R.string.assemblymanlist_name, R.string.assemblymanlist_name);
         dlDrawer.setDrawerListener(dtToggle);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_assemblymanlist, menu);
         return true;
     }
 
@@ -60,5 +75,31 @@ public class AssemblymanListActivity  extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position,
+                                long id) {
+            switch (position) {
+                case 0:
+                    flContainer.setBackgroundColor(Color.parseColor("#A52A2A"));
+                    break;
+                case 1:
+                    flContainer.setBackgroundColor(Color.parseColor("#5F9EA0"));
+                    break;
+                case 2:
+                    flContainer.setBackgroundColor(Color.parseColor("#556B2F"));
+                    break;
+                case 3:
+                    flContainer.setBackgroundColor(Color.parseColor("#FF8C00"));
+                    break;
+                case 4:
+                    flContainer.setBackgroundColor(Color.parseColor("#DAA520"));
+                    break;
+            }
+            dlDrawer.closeDrawer(lvNavList); // 추가됨
+        }
     }
 }
